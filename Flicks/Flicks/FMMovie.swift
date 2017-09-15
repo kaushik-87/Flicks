75724838f1585cf0b567631a7496c90928c85a9c
Kaushik <urskaush@gmail.com>
2017-09-15 08:26:00 -0700

    Adding SWIFTYJSON

--- Flicks/Flicks/FMMovie.swift
+++ Flicks/Flicks/FMMovie.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SwiftyJSON
 
 class FMMovie: NSObject {
     var origTitle = ""
@@ -16,24 +17,34 @@ class FMMovie: NSObject {
     var posterPath = ""
     var backdropPath = ""
     init(withMovieDictionary dictionary : NSDictionary ) {
-        if let originalTitle = dictionary[Constants.original_title] as? String {
-            self.origTitle = originalTitle
-        }
         
-        if let overView = dictionary[Constants.overview] as? String {
-            self.overView = overView
-        }
-        if let title = dictionary[Constants.title] as? String {
-            self.title = title
-        }
-        if let releaseDate = dictionary[Constants.release_date] as? String {
-            self.releaseDate = releaseDate
-        }
-        if let posterPath = dictionary[Constants.poster_path] as? String {
-            self.posterPath = posterPath
-        }
-        if let backdropPath = dictionary[Constants.backdrop_path] as? String {
-            self.backdropPath = backdropPath
-        }
+        let json = JSON(dictionary)
+        self.origTitle = json[Constants.original_title].stringValue
+        self.overView  = json[Constants.overview].stringValue
+        self.title     = json[Constants.title].stringValue
+        self.releaseDate     = json[Constants.release_date].stringValue
+        self.posterPath     = json[Constants.poster_path].stringValue
+        self.backdropPath     = json[Constants.backdrop_path].stringValue
+
+
+//        if let originalTitle = dictionary[Constants.original_title] as? String {
+//            self.origTitle = originalTitle
+//        }
+        
+//        if let overView = dictionary[Constants.overview] as? String {
+//            self.overView = overView
+//        }
+//        if let title = dictionary[Constants.title] as? String {
+//            self.title = title
+//        }
+//        if let releaseDate = dictionary[Constants.release_date] as? String {
+//            self.releaseDate = releaseDate
+//        }
+//        if let posterPath = dictionary[Constants.poster_path] as? String {
+//            self.posterPath = posterPath
+//        }
+//        if let backdropPath = dictionary[Constants.backdrop_path] as? String {
+//            self.backdropPath = backdropPath
+//        }
     }
 }
